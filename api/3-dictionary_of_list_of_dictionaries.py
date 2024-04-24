bd150aa42e891455b49da2943ce5458dc3503c6b
Sakina Mammadzade <96643702+Sakhinamammadzade@users.noreply.github.com>
2024-04-24 16:38:21 +0400

    Update 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-"""export data"""
+"""
+Saving to CSV
+"""
 
+if __name__ == '__main__':
+    import requests
+    import json
 
-import json
-import requests
+    user_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={:d}"
 
+    ids_resp = requests.get(user_url).json()
+    ids = [
+            {
+                "id": int(resp.get("id")), "username": resp.get("username")
+                } for resp in ids_resp
+            ]
 
-def export_data():
-    """saving all data to json file"""
-    users_route = 'https://jsonplaceholder.typicode.com/users'
-    todos_route = 'https://jsonplaceholder.typicode.com/todos/?userID={}'
-    users = requests.get(users_route).json()
-    data = {}
+    final_dict = dict()
 
-    for user in users:
-        user_id = user.get('id')
-        username = user.get('username')
-        todos = todos_route.format(user_id)
-        todo_request = requests.get(todos).json()
-        tasks = []
-        for todo in todo_request:
-            task = {"username": username,
-                    "task": todo.get('title'),
-                    "completed": todo.get('completed')}
-            tasks.append(task)
-        data[user_id] = tasks
+    for obj in ids:
+        todos = requests.get(todos_url.format(obj["id"])).json()
+        tasks = [{
+            "username": obj["username"],
+            "task": todo.get("title"),
+            "completed": todo.get("completed")
+            } for todo in todos]
+        final_dict |= {str(obj["id"]): tasks}
 
-    with open('todo_all_employess.json', 'w') as file:
-        json.dump(data, file)
-
-
-if __name__ == "__main__":
-    export_data()
+    with open("todo_all_employees.json", "w") as fp:
+        fp.write(json.dumps(final_dict))
